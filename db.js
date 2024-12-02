b85b149f831456d63549458e1362740ef26eeebd
SArun07 <arunsingh62392@gmail.com>
2024-12-02 14:45:03 +0530

    deploy to ec2 first-time

--- db.js
+++ db.js
@@ -1,10 +1,37 @@
-const mongoose= require('mongoose');
-const mongoURI = "mongodb://localhost:27017/taskm";
+const mongoose = require('mongoose');
 
-const connetcToMongo = () =>{
-    mongoose.connect(mongoURI, ()=>{
-        console.log("Connected to Mongo Successfully")
-    })
-}
+// Use environment variables to configure the MongoDB URI
+const password = encodeURIComponent(process.env.MONGO_PASSWORD?.trim());
+const MONGO_URI =`mongodb+srv://arunsingh0789:${password}@taskcluster.fzu6s.mongodb.net/?retryWrites=true&w=majority&appName=TaskCluster`;
 
-module.exports = connetcToMongo;
+const mongoURI = MONGO_URI || "mongodb://localhost:27017/taskm";
+
+// Function to connect to MongoDB
+const connectToMongo = async () => {
+    try {
+        await mongoose.connect(mongoURI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("Connected to MongoDB successfully!");
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error.message);
+        process.exit(1); // Exit the process with failure
+    }
+};
+
+module.exports = connectToMongo;
+
+
+//  The below code for local system
+
+// const mongoose= require('mongoose');
+// const mongoURI = "mongodb://localhost:27017/taskm";
+
+// const connetcToMongo = () =>{
+//     mongoose.connect(mongoURI, ()=>{
+//         console.log("Connected to Mongo Successfully")
+//     })
+// }
+
+// module.exports = connetcToMongo;
